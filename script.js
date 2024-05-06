883c068832182883ed2740227429ac69f83bb08a
Hiren Sarvaiya <hirensarvaiya1612@gmail.com>
2024-05-06 17:49:57 +0530

    the final commit (i guess)

--- script.js
+++ script.js
@@ -49,12 +49,12 @@ const updateSongList = () => {
   songOL.innerHTML = "";
   for (const song of songs) {
     songOL.innerHTML += `<li class="flex items-center">
-                          <img src="images/music.png" height="48px">
+                          <img src="images/music.png" class="music" height="48px">
                           <div class="info">
                             <div class="sName">${song}</div>
                             <div class="sArtist">Hiren Sarvaiya</div>
                           </div>
-                          <img src="images/playsong.png" class="playButton cur-p" height="30px">
+                          <img src="images/playsong.png" class="playButton cur-p invert1" height="30px">
                         </li>`;
   }
   
@@ -73,12 +73,12 @@ async function updateCardContainer() {
     const e = array[index];
     if (e.href.includes("/songs")) {
       let folder = e.href.split("/").slice("-2")[0];
-      let data = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`);
+      let data = await fetch(`http://127.0.0.1:3000/songs/${folder}/folderInfo/info.json`);
       let response = await data.json();
 
       cardContainer.innerHTML += `<div data-folder="${folder}" class="card cur-p">
                                     <div class="play flex justify-center items-center"><img src="images/play.png"></div>
-                                    <img src="songs/${folder}/cover.png" alt="image">
+                                    <img src="songs/${folder}/folderInfo/cover.png" alt="image">
                                     <h2>${response.title}</h2>
                                     <p>${response.description}</p>
                                   </div>`;
